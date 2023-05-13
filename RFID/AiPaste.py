f4333fdbeefa94df3fee5975d0933149cccd9de7
Al3xAda <alexander.janzen15@outlook.de>
2023-05-13 22:18:14 +0200

    chat gtp paste for research

---
+++ RFID/AiPaste.py
@@ -0,0 +1,29 @@
+#ACHTUNG: Kopierter, nicht angepasster unvollstänfig Kommentierter Code !
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+#Erlaube 'string' für ROS2 per Package
+from std_msgs.msg import String
+#Fähigkeit zu Erstellung von nodes ect.
+import rclpy
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = rclpy.create_node('rc522_reader')
+
+    pub = node.create_publisher(String, 'rfid_tag', 10)
+
+    reader = SimpleMFRC522()
+
+    try:
+        while True:
+            id, text = reader.read()
+            msg = String()
+            msg.data = str(id)
+            pub.publish(msg)
+            node.get_logger().info('Tag ID: %s' % id)
+    finally:
+        GPIO.cleanup()
+
+if __name__ == '__main__':
+    main()
